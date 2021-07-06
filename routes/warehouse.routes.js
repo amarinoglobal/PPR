af02edfc9f1c46af56caa3d36083a6c486fed102
amarinoglobal <81358493+amarinoglobal@users.noreply.github.com>
2021-07-06 18:14:00 +0200

    warehouse routes

--- routes/warehouse.routes.js
+++ routes/warehouse.routes.js
@@ -5,6 +5,15 @@ const Warehouse = require('./../models/Warehouse.model')
 
 //Register Warehouse
 
+router.get('/', (req, res, next) => {
+
+    Warehouse
+        .find({ owner: req.session.currentUser })
+        // .then(warehouse => res.render('/movies', { movies }))
+
+        .catch(err => console.log(err))
+})
+
 router.get('/registro-almacen', (req, res) => res.render('warehouse/new-warehouse'))
 
 router.post('/registro-almacen', (req, res) => {
@@ -21,10 +30,39 @@ router.post('/registro-almacen', (req, res) => {
 
     Warehouse
         .create({ name, owner: req.session.currentUser, location })
-        // .then(() => res.redirect('/restaurantes/lista'))
+        // .then(() => res.redirect('/warehouse'))
         .then(() => res.send(req.body))
         .catch(err => console.log(err))
 
 })
+
+router.get('/detalles/:warehouse_id', (req, res) => {
+
+    const { warehouse_id } = req.params
+
+    Warehouse
+        .findById(warehouse_id)
+        .then(warehouse => res.render('warehouse/warehouse-details', warehouse))
+        .catch(err => console.log(err))
+})
+
+router.get('/:warehouse_id/editar', (req, res) => {
+
+    const { warehouse_id } = req.params
+
+    Movie
+        .findById(warehouse_id)
+        .then(movies => res.render('warehouse/edit-warehouse', warehouse))
+
+        .catch(err => console.log(err))
+
+
+})
+
+
+
+
+
+
 
 module.exports = router
